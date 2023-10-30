d6212730a3c26172ac5a0021f842764edb7c5338
sophialexis <149432475+sophialexis@users.noreply.github.com>
2023-10-30 16:25:27 -0400

    Add files via upload

---
+++ TP.py
@@ -0,0 +1,21 @@
+import random
+import pickle
+
+pseudo=input ("entrer un pseudo")
+if espedo=espedo.replace("","")
+if espedo.issabi() and len(psedo)>=6 
+print(" non respecte")
+n=random,randint ( 0,100)
+
+
+for i in range (5):
+X=int(input ("donne un nombre un intervalle de 0 a 100"))
+if X<n:
+
+nombre_saisi = int( input (" entrer un nombre de l'intervalle de 0 a 100"))
+if nombre_saisi= nombre_secret:
+    print ("BRAVO, vous avez devine le nombre secret.")
+elif nombre_saisi < nombre_secret:
+    print (" desole, le nombre saisi est trop petit, choisi un plus grand nombre. ")
+else:
+    print(" Le nombre saisi est trop grand, choisi un plus petit nombre")
